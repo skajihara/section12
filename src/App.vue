7b3f1e0b1102d1e6b092b4863a966f0dd2433d03
skajihara <t15cs015@gmail.com>
2024-04-01 15:39:34 +0900

    section12-12

--- src/App.vue
+++ src/App.vue
@@ -24,11 +24,15 @@ const newFruit = ref('')
 .fade-enter-to {
   opacity: 1;
 }
+.fade-move {
+  transition: transform 1s;
+}
 .fade-leave-from {
   opacity: 1;
 }
 .fade-leave-active {
   transition: opacity 1s;
+  position: absolute;
 }
 .fade-leave-to {
   opacity: 0;
