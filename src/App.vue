932d46481287f81d747729416afe73879621f151
skajihara <t15cs015@gmail.com>
2024-04-01 15:19:03 +0900

    section12-11

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,18 @@
 <script setup>
 import { ref } from 'vue'
 
-const isShow = ref(true)
-function beforeEnter(el) {
-  console.log('beforeEnter', el)
-  el.style.transform = 'translateX(30px)'
-}
-let enterIntervalId
-function enter(el, done) {
-  console.log('enter', el)
-  let translateXValue = 30
-  enterIntervalId = setInterval(() => {
-    translateXValue -= 1
-    el.style.transform = `translateX(${translateXValue}px)`
-    if (translateXValue === 0) {
-      clearInterval(enterIntervalId)
-      done()
-    }
-  }, 20)
-}
-function afterEnter(el) {
-  console.log('afterEnter', el)
-}
-function enterCancelled(el) {
-  console.log('enterCancelled', el)
-  clearInterval(enterIntervalId)
-}
-function beforeLeave(el) {
-  console.log('beforeLeave', el)
-}
-let leaveIntervalId
-function leave(el, done) {
-  console.log('leave', el)
-  let translateXValue = 0
-  leaveIntervalId = setInterval(() => {
-    translateXValue += 1
-    el.style.transform = `translateX(${translateXValue}px)`
-    if (translateXValue === 30) {
-      clearInterval(leaveIntervalId)
-      done()
-    }
-  }, 20)
-}
-function afterLeave(el) {
-  console.log('afterLeave', el)
-}
-function leaveCancelled(el) {
-  console.log('leaveCancelled', el)
-  clearInterval(leaveIntervalId)
-}
+const fruits = ref(['Apple', 'Banana', 'Grape'])
+const newFruit = ref('')
 </script>
 <template>
   <h1>Animation</h1>
-  <button @click="isShow = !isShow">switch</button>
-  <Transition
-    :css="false"
-    @before-enter="beforeEnter"
-    @enter="enter"
-    @after-enter="afterEnter"
-    @enter-cancelled="enterCancelled"
-    @before-leave="beforeLeave"
-    @leave="leave"
-    @after-leave="afterLeave"
-    @leave-cancelled="leaveCancelled"
-  >
-    <div v-show="isShow">Hello</div>
-  </Transition>
+  <input v-model="newFruit" type="text" />
+  <button @click="fruits.unshift(newFruit)">Add</button>
+  <TransitionGroup name="fade" tag="div">
+    <div v-for="(fruit, index) in fruits" :key="fruit" @click="fruits.splice(index, 1)">
+      {{ fruit }}
+    </div>
+  </TransitionGroup>
 </template>
 <style scoped>
 .fade-enter-from {
